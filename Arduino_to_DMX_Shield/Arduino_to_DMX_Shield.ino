b3024e62bd50f596244c42849518637a8ea81e05
Roope Salmi <rpsalmi@gmail.com>
2015-01-31 20:31:21 +0200

    Completely re-do the Arduino protocol ...for a stupid reason (might be reverted)

--- Arduino_to_DMX_Shield/Arduino_to_DMX_Shield.ino
+++ Arduino_to_DMX_Shield/Arduino_to_DMX_Shield.ino
@@ -1,72 +1,56 @@
 //(Based on:)DMX communication protocol for Processing ---> Arduino
 //Made by Roope Salmi
 
-#include <DmxSimple.h>
+
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(6, 6, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("SerialToDMXshield ready for commands");
   Serial.println("Protocol: https://gist.github.com/ollpu/239cc50beb91bfe06189");
+  pixels.begin();
 }
 
-byte message[3];
-byte pointer = 0;
+int value = 0;
+int channel;
+boolean channelDeclared = false;
 
 void loop() {
-  
-  
-  if(Serial.available() > 0) {
-    if(Serial.peek() >= 250) {
-      pointer = 0;
-      parseMessage(message);
+  int c;
+
+  while(!Serial.available());
+  c = Serial.read();
+  if ((c>=0) && (c<=31)) {
+    value = 32*value + c;
+  } else {
+    if (c==0xFE) { channel = value; channelDeclared = true; }
+    else if (c==0xFF && channelDeclared) {
+      execute(channel+1, value);
+      
     }
-    message[pointer] = Serial.read();
-    pointer++;
+    value = 0;
   }
-  
-  
 }
 
-void parseMessage(byte* mesg) {
-  if(mesg[0] == 255 && mesg[2] > 5) {
-    //Configuration command
-    configurationCommand(mesg[2], mesg[1]);
-  } else
-  switch(mesg[0]) {
-    case 250:
-      execute(mesg[1]+1, mesg[2]);
-    break;
-    case 251:
-      execute(mesg[1]+1, mesg[2] + 250);
-    break;
-    case 252:
-      execute(mesg[1]+1 + 250, mesg[2]);
-    break;
-    case 253:
-      execute(mesg[1]+1 + 250, mesg[2] + 250);
-    break;
-    case 254:
-      execute(mesg[1]+1 + 262, mesg[2]);
-    break;
-    case 255:
-      execute(mesg[1]+1 + 262, mesg[2] + 250);
-    break;
-  }
-}
 
 void execute(int channel, int data) {
   //Now you can do whatever you like with this data
-  DmxSimple.write(channel, data);
-  Serial.write(channel);
+  //DmxSimple.write(channel, data);
+  //Serial.write(channel);
+  pixels.setPixelColor(channel-1, data, data, data);
+  pixels.show();
+  Serial.write(data);
 }
 
 void configurationCommand(byte type, byte data) {
   switch(type) {
     case 6:
-      DmxSimple.maxChannel((data+1)*2);
+      //DmxSimple.maxChannel((data+1)*2);
     break;
     case 7:
-      DmxSimple.maxChannel((data+1+250)*2);
+      //DmxSimple.maxChannel((data+1+250)*2);
     break;
   }
 }
